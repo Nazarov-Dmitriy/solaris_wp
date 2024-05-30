1c16cbca6a233b66b97dec390aab266bbd159651
Nazarov-Dmitriy <Difat1@inbox.ru>
2024-05-31 00:08:56 +0300

    история покупок

--- wp-content/themes/solaris/template-parts/cabinet-user/user-shop-sales.php
+++ wp-content/themes/solaris/template-parts/cabinet-user/user-shop-sales.php
@@ -25,6 +25,49 @@
             "img" => "/assets/image/cabinet-shop/icon.png",
         ],
     ];
+
+    $infoBuyHistoryItem = [
+        [
+            "imgSrc" => "/assets/image/cabinet-shop/solaris2.png",
+            "amount" => 200,
+            "orderNumber" => "Стикеры солярики № 33500967",
+            "orderStatus" => "заказ оплачен",
+            "time" => "13.02.2024",
+            "img_valute" => "/assets/image/cabinet-shop/currency.png",
+        ],
+        [
+            "imgSrc" => "/assets/image/cabinet-shop/solaris2.png",
+            "amount" => 150,
+            "orderNumber" => "Антидвойка № 33500967",
+            "orderStatus" => "заказ оплачен",
+            "time" => "13.02.2024",
+            "img_valute" => "/assets/image/cabinet-shop/currency.png",
+        ],
+        [
+            "imgSrc" => "/assets/image/cabinet-shop/solaris2.png",
+            "amount" => 150,
+            "orderNumber" => "Антидвойка № 33500967",
+            "orderStatus" => "заказ оплачен",
+            "time" => "13.02.2024",
+            "img_valute" => "/assets/image/cabinet-shop/currency.png",
+        ],
+        [
+            "imgSrc" => "/assets/image/cabinet-shop/solaris2.png",
+            "amount" => 150,
+            "orderNumber" => "Стикеры солярики № 33500967",
+            "orderStatus" => "заказ оплачен",
+            "time" => "13.02.2024",
+            "img_valute" => "/assets/image/cabinet-shop/currency.png",
+        ],
+        [
+            "imgSrc" => "/assets/image/cabinet-shop/solaris2.png",
+            "amount" => 150,
+            "orderNumber" => "Стикеры солярики № 33500967",
+            "orderStatus" => "заказ оплачен",
+            "time" => "13.02.2024",
+            "img_valute" => "/assets/image/cabinet-shop/currency.png",
+        ],
+    ];
     ?>
 
   <section class="cabinet-shop">
@@ -39,48 +82,56 @@
                   </p>
                   <h2 class="cabinet-shop-ready__login">IvanovAI_7a</h2>
               </div>
-
-              <ul class="cabinet-shop-ready__list">
-                  <?php
-                    foreach ($infoBuyItemInfo as $value) { ?>
-                      <li class="cabinet-shop-ready__list-item">
-                          <div class="cabinet-shop-ready__list-item-img">
-                              <img src="<?php echo get_template_directory_uri()?><?php echo $value['img']?>" alt="image" />
-                          </div>
-                          <div class="cabinet-shop-ready__list-item-info">
-                              <p class="list-item-info"><?php echo $value['name']?></p>
-                              <p class="list-item-info"><?php echo $value['number']?></p>
-                              <p class="list-item-info">Получить</p>
-                              <p class="list-item-info"><?php echo $value['text']?></p>
-                          </div>
-                      </li>
-                  <?php  } ?>
-              </ul>
+              <?php
+                if ($infoBuyItemInfo) {
+                ?>
+                  <ul class="cabinet-shop-ready__list">
+                      <?php
+                        foreach ($infoBuyItemInfo as $value) { ?>
+                          <li class="cabinet-shop-ready__list-item">
+                              <div class="cabinet-shop-ready__list-item-img">
+                                  <img src="<?php echo get_template_directory_uri() ?><?php echo $value['img'] ?>" alt="image" />
+                              </div>
+                              <div class="cabinet-shop-ready__list-item-info">
+                                  <p class="list-item-info"><?php echo $value['name'] ?></p>
+                                  <p class="list-item-info"><?php echo $value['number'] ?></p>
+                                  <p class="list-item-info">Получить</p>
+                                  <p class="list-item-info"><?php echo $value['text'] ?></p>
+                              </div>
+                          </li>
+                      <?php  } ?>
+                  </ul>
+              <?php } else { ?>
+                  <p class="shop-empty-ready"> У тебя сейчас нет товаров готовых к получению, ты можешь выбрать их в магазине</p>
+              <?php } ?>
           </div>
 
           <div class="cabinet-shop-history">
               <p class="cabinet-shop-history__title">История покупок</p>
-              <ul class="cabinet-shop-history__wrapper">
-                  <!-- <li class="cabinet-shop-history__wrapper-item">
-            <div class="cabinet-shop-history__wrapper-left">
-              <img
-                src="../../../assets/image/cabinet-shop/solaris2.png"
-                alt="картинка"
-              />
-              <p class="cabinet-shop-history__wrapper-price">200</p>
-              <img
-                src="../../../assets/image/cabinet-shop/currency.png"
-                alt=""
-              />
-            </div>
-            <div class="cabinet-shop-history__wrapper-info">
-              <p>Антидвойка</p>
-              <p>13.12.2024 <span>заказ оплачен</span></p>
-            </div>
-          </li> -->
-              </ul>
+              <?php
+                if ($infoBuyHistoryItem) {
+                ?>
+                  <ul class="cabinet-shop-history__wrapper">
+                      <?php
+                        foreach ($infoBuyHistoryItem as $value) { ?>
+                          <li class="cabinet-shop-history__wrapper-item">
+                              <div class="cabinet-shop-history__wrapper-left">
+                                  <img src="<?php echo get_template_directory_uri() ?><?php echo $value['imgSrc'] ?>" alt="<?php echo $value['orderNumber'] ?>" />
+                                  <p class="cabinet-shop-history__wrapper-price"><?php echo $value['amount'] ?> </p>
+                                  <img src="<?php echo get_template_directory_uri() ?><?php echo $value['img_valute'] ?>" alt="solaris-valute" />
+                              </div>
+                              <div class="cabinet-shop-history__wrapper-info">
+                                  <p><?php echo $value['orderNumber'] ?></p>
+                                  <p><?php echo $value['time'] ?> <span>заказ оплачен</span></p>
+                              </div>
+                          </li>
+                      <?php  } ?>
+                  </ul>
+              <?php } else { ?>
+                  <p class="shop-empty-history">Здесь отображаются полученные товары, видимо ты пока ничего не покупал</p>
+              <?php } ?>
               <div class="cabinet-shop__img-background">
-                  <img src="../../../assets/image/cabinet-shop/coinBege.png" alt="" />
+                  <img src="<?php echo get_template_directory_uri() ?>/assets/image/cabinet-shop/coinBege.png" alt="" />
               </div>
           </div>
       </div>
